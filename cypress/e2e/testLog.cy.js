529ce50aa25c4e3abbe2432f0b4a53026a989a65
LucynaPelc <lucynapelc.project@gmail.com>
2024-12-05 17:22:22 +0100

    fix4 logTest

--- cypress/e2e/testLog.cy.js
+++ cypress/e2e/testLog.cy.js
@@ -8,8 +8,7 @@ describe("user visit GoIT page", () => {
       cy.get('#user_password').type("1234567890");
       cy.get('.eckniwg2').click();
   
-      // Sprawdź, czy dashboard jest widoczny
-    //   cy.get(".next-vhmemw > .MuiBox-root").should('be.visible').click();
+    
       cy.get('#open-navigation-menu-mobile').should('be.visible').click();
       // Czekaj na element lub użyj precyzyjniejszego selektora
       cy.get(':nth-child(12) > .next-bve2vl', { timeout: 10000 }).should('exist').click();
